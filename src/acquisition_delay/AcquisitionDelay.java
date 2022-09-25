4f8bcee5563c293ef94bab7851b1866aeb689194
Didier Walliang <didier.walliang@gmail.com>
2022-09-25 11:43:55 +0200

    First commit

---
+++ src/acquisition_delay/AcquisitionDelay.java
@@ -0,0 +1,9 @@
+package acquisition_delay;
+
+public class AcquisitionDelay {
+	
+	public void calculate() {
+		System.out.println("Hello");
+	}
+	
+}
---
+++ src/acquisition_delay/Launch.java
@@ -0,0 +1,10 @@
+package acquisition_delay;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		AcquisitionDelay acquisitionDelay = new AcquisitionDelay();
+		acquisitionDelay.calculate();
+	}
+
+}
