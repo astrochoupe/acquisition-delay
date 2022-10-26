b5dc0effef2868c15210f4c94be32ea0596e7192
Didier Walliang <didier.walliang@gmail.com>
2022-10-27 00:17:35 +0200

    Refactoring. Create BigDecimalUtils.java.

--- src/acquisition_delay/AcquisitionDelay.java
+++ src/acquisition_delay/AcquisitionDelay.java
@@ -6,7 +6,6 @@ import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class AcquisitionDelay {
@@ -224,19 +223,19 @@ public class AcquisitionDelay {
 		}
 
 		if (!timesPpsStart.isEmpty()) {
-			BigDecimal averageTimePpsStart = average(timesPpsStart, 1);
-			BigDecimal rmsTimePpsStart = rootMeanSquare(timesPpsStart, averageTimePpsStart, 1);
+			BigDecimal averageTimePpsStart = BigDecimalUtils.average(timesPpsStart, 1);
+			BigDecimal rmsTimePpsStart = BigDecimalUtils.rootMeanSquare(timesPpsStart, averageTimePpsStart, 1);
 			System.out.print("List of times PPS start: ");
-			print(timesPpsStart, 1);
+			BigDecimalUtils.print(timesPpsStart, 1);
 			System.out.println();
 			System.out.println("Average time PPS start: " + averageTimePpsStart + " ms ± " + rmsTimePpsStart);
 		}
 
 		if (!timesPpsEnd.isEmpty()) {
-			BigDecimal averageTimePpsEnd = average(timesPpsEnd, 1);
-			BigDecimal rmsTimePpsEnd = rootMeanSquare(timesPpsEnd, averageTimePpsEnd, 1);
+			BigDecimal averageTimePpsEnd = BigDecimalUtils.average(timesPpsEnd, 1);
+			BigDecimal rmsTimePpsEnd = BigDecimalUtils.rootMeanSquare(timesPpsEnd, averageTimePpsEnd, 1);
 			System.out.print("List of times PPS end: ");
-			print(timesPpsEnd, 1);
+			BigDecimalUtils.print(timesPpsEnd, 1);
 			System.out.println();
 			System.out.println("Average time PPS end: " + averageTimePpsEnd + " ms ± " + rmsTimePpsEnd);
 		}
@@ -246,65 +245,6 @@ public class AcquisitionDelay {
 		double doubleValue;
 		doubleValue = Double.parseDouble(decimalString);
 		return (int) doubleValue;
-	}
-	
-	private BigDecimal average(List<BigDecimal> list, int scale) {
-		BigDecimal[] totalWithCount = list.stream().filter(bd -> bd != null)
-				.map(bd -> new BigDecimal[] { bd, BigDecimal.ONE })
-				.reduce((a, b) -> new BigDecimal[] { a[0].add(b[0]), a[1].add(BigDecimal.ONE) }).get();
-		return totalWithCount[0].divide(totalWithCount[1], scale, RoundingMode.HALF_UP);
-	}
-	
-	/**
-	 * Calculate the root mean square of a list of BigDecimal.
-	 * 
-	 * @param list a list of BigDecimal
-	 * @param mean the average of the list
-	 * @param scale scale of the BigDecimal quotient to be returned
-	 * @return zero if list is null or empty or if mean if null
-	 */
-	private BigDecimal rootMeanSquare(List<BigDecimal> list, BigDecimal mean, int scale) {
-		if(list != null && !list.isEmpty() && mean != null) {
-			BigDecimal variance = variance(list, mean);
-			BigDecimal rootMeanSquare = variance.divide(BigDecimal.valueOf(list.size()), scale, RoundingMode.HALF_UP);
-			return rootMeanSquare;
-		} else {
-			return BigDecimal.ZERO;
-		}	
-	}
-	
-	/**
-	 * Calculate the variance of a list of BigDecimal.
-	 * 
-	 * @param list a list of BigDecimal
-	 * @param mean the average of the list
-	 * @return zero if list is null or empty or if mean if null
-	 */
-	private BigDecimal variance(List<BigDecimal> list, BigDecimal mean) {
-		BigDecimal variance = BigDecimal.ZERO;
-		
-		if(list != null && !list.isEmpty() && mean != null) {
-			for(BigDecimal number : list) {
-				variance = variance.add(number.subtract(mean).pow(2));
-			}
-		}
-		
-		return variance;
-	}
-	
-	private void print(List<BigDecimal> list, int scale) {
-		System.out.print("[");
-		
-		Iterator<BigDecimal> iterator = list.iterator();
-		while(iterator.hasNext()) {
-			BigDecimal bigDecimal = iterator.next();
-			System.out.print(bigDecimal.setScale(scale, RoundingMode.HALF_UP));
-			if(iterator.hasNext()) {
-				System.out.print(", ");
-			}
-		}
-		
-		System.out.print("]");
 	}
 
 }
---
+++ src/acquisition_delay/BigDecimalUtils.java
@@ -0,0 +1,90 @@
+package acquisition_delay;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Provides utility methods for {@link BigDecimal} instances.
+ */
+public class BigDecimalUtils {
+
+	/**
+	 * <code>BigDecimalUtils</code> should not normally be instantiated.
+	 */
+	private BigDecimalUtils() {
+	}
+
+	/**
+	 * Calculate the average of a list of BigDecimal.
+	 * 
+	 * @param list  a list of BigDecimal
+	 * @param scale scale of the BigDecimal quotient to be returned
+	 * @return the average of the list passed as parameter.
+	 */
+	public static BigDecimal average(List<BigDecimal> list, int scale) {
+		BigDecimal[] totalWithCount = list.stream().filter(bd -> bd != null)
+				.map(bd -> new BigDecimal[] { bd, BigDecimal.ONE })
+				.reduce((a, b) -> new BigDecimal[] { a[0].add(b[0]), a[1].add(BigDecimal.ONE) }).get();
+		return totalWithCount[0].divide(totalWithCount[1], scale, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Calculate the root mean square of a list of BigDecimal.
+	 * 
+	 * @param list  a list of BigDecimal
+	 * @param mean  the average of the list
+	 * @param scale scale of the BigDecimal quotient to be returned
+	 * @return zero if list is null or empty or if mean if null
+	 */
+	public static BigDecimal rootMeanSquare(List<BigDecimal> list, BigDecimal mean, int scale) {
+		if (list != null && !list.isEmpty() && mean != null) {
+			BigDecimal variance = variance(list, mean);
+			BigDecimal rootMeanSquare = variance.divide(BigDecimal.valueOf(list.size()), scale, RoundingMode.HALF_UP);
+			return rootMeanSquare;
+		} else {
+			return BigDecimal.ZERO;
+		}
+	}
+
+	/**
+	 * Calculate the variance of a list of BigDecimal.
+	 * 
+	 * @param list a list of BigDecimal
+	 * @param mean the average of the list
+	 * @return zero if list is null or empty or if mean if null
+	 */
+	public static BigDecimal variance(List<BigDecimal> list, BigDecimal mean) {
+		BigDecimal variance = BigDecimal.ZERO;
+
+		if (list != null && !list.isEmpty() && mean != null) {
+			for (BigDecimal number : list) {
+				variance = variance.add(number.subtract(mean).pow(2));
+			}
+		}
+
+		return variance;
+	}
+
+	/**
+	 * Print a list of BigDecimal.
+	 * 
+	 * @param list  a list of BigDecimal to print
+	 * @param scale scale of the BigDecimal quotient to be returned
+	 */
+	public static void print(List<BigDecimal> list, int scale) {
+		System.out.print("[");
+
+		Iterator<BigDecimal> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			BigDecimal bigDecimal = iterator.next();
+			System.out.print(bigDecimal.setScale(scale, RoundingMode.HALF_UP));
+			if (iterator.hasNext()) {
+				System.out.print(", ");
+			}
+		}
+
+		System.out.print("]");
+	}
+}
